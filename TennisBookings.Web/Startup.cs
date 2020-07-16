12bf5f6df51315c4af8121889ce3d46b053ce493
lorenzo-dallamuta <lorenzodallamuta@gmail.com>
2020-07-16 14:26:10 +0200

    methods of IServiceCollection

--- TennisBookings.Web/Startup.cs
+++ TennisBookings.Web/Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;  //no idea why this is needed if I use the higher node too, but it's required for .TryAdd
 using Microsoft.Extensions.Hosting;
 using TennisBookings.Web.Configuration;
 using TennisBookings.Web.Services;
@@ -25,7 +21,23 @@ namespace TennisBookings.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IWeatherForecaster, WeatherForecaster>();
+            //services.Configure<ExternalServicesConfig>(Configuration.GetSection("ExternalServices"));
+
+            //services.AddHttpClient<IWeatherApiClient, WeatherApiClient>();
+
+            services.AddSingleton<IWeatherForecaster, WeatherForecaster>();
+
+            //var serviceDescriptor = new ServiceDescriptor(typeof(IWeatherForecaster), typeof(WeatherForecaster), ServiceLifetime.Singleton);
+            //var serviceDescriptor = ServiceDescriptor.Describe(typeof(IWeatherForecaster), typeof(WeatherForecaster), ServiceLifetime.Singleton);
+            //var serviceDescriptor = ServiceDescriptor.Singleton(typeof(IWeatherForecaster), typeof(WeatherForecaster));
+            //var serviceDescriptor = ServiceDescriptor.Singleton<IWeatherForecaster, WeatherForecaster>();
+            //services.Add(serviceDescriptor);
+
+            //services.AddSingleton<IWeatherForecaster, AmazingWeatherForecaster>();
+            //services.TryAddSingleton<IWeatherForecaster, AmazingWeatherForecaster>();
+            //services.Replace(ServiceDescriptor.Singleton<IWeatherForecaster, AmazingWeatherForecaster>());
+            //services.RemoveAll<IWeatherForecaster>();
+
 
             services.Configure<FeaturesConfiguration>(Configuration.GetSection("Features"));
 
